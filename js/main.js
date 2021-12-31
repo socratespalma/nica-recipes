0b61f4d4c63f75c4b0f46d6e58a27885a4faa093
Socrates Palma <dubstepero10@gmail.com>
2021-12-30 20:15:01 -0600

    finished all pages except single-recipe page

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
-// nav toggle -> select button and links
+// DOM selectors
 const navToggle = document.querySelector("#navToggle");
 const nav = document.querySelector("#nav-links");
 const hamburger = document.querySelector(".menu");
 const close = document.querySelector(".close");
+const submitBtn = document.querySelector(".submit");
 
-// add event listener
-navToggle.addEventListener("click", () => {
+// functions
+// toggles navbar while clicking hamburger button
+const toggleNav = () => {
   nav.classList.toggle("nav-open");
   hamburger.classList.toggle("hide");
   close.classList.toggle("show");
-});
+};
+
+// prevents reloading the page
+const handleSubmit = (e) => {
+  e.preventDefault();
+};
+
+// callbacks
+navToggle.addEventListener("click", toggleNav);
+submitBtn.addEventListener("click", handleSubmit);
